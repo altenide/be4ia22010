e840895031931ed43f8bf9093611d4dfdd803e86
aurelien.mariage <aurelien.mariage@ccd54480-e988-11de-8de6-035db03795fd>
2010-01-11 15:57:37 +0000

    Test de fusion de l'ihm et du réseau sur le terminal mobile.
    Personnes autorisées:
    Edith,
    Arthur,
    Sylvain,
    Aurélien

---
+++ TestFinal/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gui.IHM ihm = new gui.IHM();
+		//gui.IHMawt ihm = new gui.IHMawt();		
+		ihm.setVisible(true);		
+	}
+
+}
