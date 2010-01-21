73d9025bf630f60e75b261b60c140d246458c5a6
arthur.daussy <arthur.daussy@ccd54480-e988-11de-8de6-035db03795fd>
2010-01-21 13:22:59 +0000

    Synthese marche

--- trunk/Serveur/ServeurFinal/src/Synthese/son/LecteurTexte.java
+++ trunk/Serveur/ServeurFinal/src/Synthese/son/LecteurTexte.java
@@ -1,287 +1,296 @@
-/*  
+/*
  * SI VOX Copyright (C) 2004 - 2005
  *
  * Author :
  *   ESSI2 school project (2004) : Affouard, Lemonnier, Fournols ,Lizzul
- *   Tutor                (2004) : H�l�ne Collavizza   [ helen@essi.fr    ] 
+ *   Tutor                (2004) : Hï¿½lï¿½ne Collavizza   [ helen@essi.fr    ]
  *                                 Jean-Paul Stromboni [ strombon@essi.fr ]
  *
  * Contributor :
  *   (2004) : Louis Parisot [ parisot@essi.fr ]
- *   (2005) : S�bastien Mosser  [ mosser@essi.fr ]
+ *   (2005) : Sï¿½bastien Mosser  [ mosser@essi.fr ]
  *
- * Institute : 
+ * Institute :
  *    Polytechnich school, University of Nice - Sophia Antipolis (FRANCE)
  *
  * This program is free software. It uses mbrola speech synthesizers system.
- * 
- * You can redistribute it and/or modify it under the terms of the MBROLA 
+ *
+ * You can redistribute it and/or modify it under the terms of the MBROLA
  * Licenses  { http://tcts.fpms.ac.be/synthesis/mbrola.html }.
  *
  */
-
 package Synthese.son;
 
-
-import java.io.*;
 import Synthese.prosodie.*;
 import Synthese.traitement.*;
 import Synthese.util.*;
-
+import java.io.*;
 
 /** Classe de lecture d'un texte
  */
+public class LecteurTexte {
 
-public class LecteurTexte{
-
-    // le path des fichiers de r�gles utilis� pour transformer les mots en phon�mes
-    private final static String PATH_REGLE=ConfigFile.rechercher("CHEMIN_REGLES");
-    // le fichier pour �crire les wav ou pho
-    private final static String PHO_WAV=ConfigFile.rechercher("FICHIER_PHO_WAV");
-
-    private String outFile ;    //le fichier pour �crire les fichiers son
-    private String voix;        // la voix utilis�e par MBROLA
-    private Arbre arbre;        // l'arbre contenant les r�gles
-    private Pretraitement pt;   // le texte apr�s pr�traitement 
-    private boolean vide;       // vrai si l'on a trait� compl�tement le texte
+    // le path des fichiers de rï¿½gles utilisï¿½ pour transformer les mots en phonï¿½mes
+    private final static String PATH_REGLE = ConfigFile.rechercher("CHEMIN_REGLES");
+    // le fichier pour ï¿½crire les wav ou pho
+    private final static String PHO_WAV = ConfigFile.rechercher("FICHIER_PHO_WAV");
+    private String outFile;    //le fichier pour ï¿½crire les fichiers son
+    private String voix;        // la voix utilisï¿½e par MBROLA
+    private Arbre arbre;        // l'arbre contenant les rï¿½gles
+    private Pretraitement pt;   // le texte aprï¿½s prï¿½traitement
+    private boolean vide;       // vrai si l'on a traitï¿½ complï¿½tement le texte
     private BufferedReader cin; // entree standard
 
-    /** Constructeur complet de Lecteur de Texte 
-     * @param s le texte � lire
-     * @param path l'emplacement des fichiers de r�gles
-     * @param of le fichier ou l'on �crit les phon�mes � prononcer
-     * @param v la voix � utiliser pour lire ces phon�mes
-     * @param isFile Lit - on depuis un fichier ou depuis l'entr�e standart ?
+    /** Constructeur complet de Lecteur de Texte
+     * @param s le texte ï¿½ lire
+     * @param path l'emplacement des fichiers de rï¿½gles
+     * @param of le fichier ou l'on ï¿½crit les phonï¿½mes ï¿½ prononcer
+     * @param v la voix ï¿½ utiliser pour lire ces phonï¿½mes
+     * @param isFile Lit - on depuis un fichier ou depuis l'entrï¿½e standart ?
      */
-    public LecteurTexte(String s,String path,  String of, String v, boolean isFile) {
-	outFile = of;
-	vide= false;
-	voix = v;
-	boolean stdin; // vrai si il faut lire sur l'entree standard
-	if(s.equals("-")) {
-	    stdin = true;
-	    cin = new BufferedReader(new InputStreamReader(System.in));
-	} else {
-	    stdin = false;
-	    String texte = (isFile)? (new LecteurFichier(s)).toutLire() : s;
-	    pt =  new Pretraitement(texte);
-	}
-	try {
-	    arbre = new Arbre(path); 
-	} catch (AnalyseException a) {
-	    System.out.println(a);
-	}
-	if(stdin)
-	    interactifMode();
+    public LecteurTexte(String s, String path, String of, String v, boolean isFile) {
+        outFile = of;
+        vide = false;
+        voix = v;
+        boolean stdin; // vrai si il faut lire sur l'entree standard
+        if (s.equals("-")) {
+            stdin = true;
+            cin = new BufferedReader(new InputStreamReader(System.in));
+        } else {
+            stdin = false;
+            String texte = (isFile) ? (new LecteurFichier(s)).toutLire() : s;
+            pt = new Pretraitement(texte);
+        }
+        try {
+            arbre = new Arbre(path);
+        } catch (AnalyseException a) {
+            System.out.println(a);
+        }
+        if (stdin) {
+            interactifMode();
+        }
     }
 
-    /** M�thode d'affichage standart
-     * @return une chaine de caract�re ad'hoc
+    /** Mï¿½thode d'affichage standart
+     * @return une chaine de caractï¿½re ad'hoc
      */
     @Override
-    public String toString(){
-	String s = "";
-	s = "  Sortie :=" + outFile +"\n";
-	return s;
+    public String toString() {
+        String s = "";
+        s = "  Sortie :=" + outFile + "\n";
+        return s;
     }
-    
-    /** Constructeur all�g� utilisant des valeurs par d�faut
-     * @param s le texte � lire
+
+    /** Constructeur allÃ©gÃ© utilisant des valeurs par dï¿½faut
+     * @param s le texte ï¿½ lire
      * @param isFile Lit-on depuis un fichier ?
      */
     public LecteurTexte(String s, boolean isFile) {
-	this(s,PATH_REGLE,PHO_WAV,SynthetiseurMbrola.VOIX1,isFile);
+        this(s, PATH_REGLE, PHO_WAV, SynthetiseurMbrola.VOIX1, isFile);
     }
 
-    /** Constructeur completement all�g�
-     * @param s le texte � lire
+    /** Construteur allÃ©gÃ© permettant de choisir le nom du fichier
+     * @param s le texte Ã  lire
+     * @param pathFile le chemin du fichier (sans l'extension)
+     */
+    public LecteurTexte(String s, String pathFile) {
+        this(s, PATH_REGLE, pathFile, SynthetiseurMbrola.VOIX1, false);
+    }
+
+    /** Constructeur completement allï¿½gï¿½
+     * @param s le texte ï¿½ lire
      */
     public LecteurTexte(String s) {
-	this(s,PATH_REGLE,PHO_WAV,SynthetiseurMbrola.VOIX1,false);
+        this(s, PATH_REGLE, PHO_WAV, SynthetiseurMbrola.VOIX1, false);
     }
 
     /** Lecteur de fichier
      */
-    public LecteurTexte(File f, String out){
-	this(f.toString(),PATH_REGLE,out,SynthetiseurMbrola.VOIX1,true);
+    public LecteurTexte(File f, String out) {
+        this(f.toString(), PATH_REGLE, out, SynthetiseurMbrola.VOIX1, true);
 
     }
 
     /** Constructeur de lecteur vide.
      */
     public LecteurTexte() {
-	this("",false);
+        this("", false);
     }
 
-    
     /** Pour lire du texte en mode interactif.
      */
     private void interactifMode() {
         try {
             String text = "";
-	    String ligne;
-	    Phrase p;
-	    SynthetiseurMbrola s;
+            String ligne;
+            Phrase p;
+            SynthetiseurMbrola s;
             while (true) {
                 ligne = cin.readLine();
-                if(ligne != null) {
+                if (ligne != null) {
                     if (ligne.equals("<QUIT>")) {
                         System.exit(0);
-                    } else if(!ligne.equals("<END>")) {
+                    } else if (!ligne.equals("<END>")) {
                         text = text + ligne;
                     } else {
                         pt = new Pretraitement(text);
                         this.muet(SynthetiseurMbrola.MBROLAHOME);
-			System.out.println(outFile + ".wav");
-			vide = false;
-			text = "";
+                        System.out.println(outFile + ".wav");
+                        vide = false;
+                        text = "";
                     }
                 } else {
                     try {
-			System.out.println("sleeping...");
+                        System.out.println("sleeping...");
                         Thread.sleep(200);
-                    } catch(InterruptedException e) {
-			System.out.println("InterruptedException in interactifMode()");
-		    }
+                    } catch (InterruptedException e) {
+                        System.out.println("InterruptedException in interactifMode()");
+                    }
                 }
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             System.out.println("<IOException>");
         }
     }
-	
-	
-    /** Pour mettre � jour la voix utilis� par le synth�tiseur
-     * @param v le num�ro de la voix (parmis 1,2 ou 3)
+
+    /** Pour mettre ï¿½ jour la voix utilisï¿½ par le synthï¿½tiseur
+     * @param v le numï¿½ro de la voix (parmis 1,2 ou 3)
      */
     public void setVoix(int v) {
-	switch (v) {
-	case 1: 
-	    voix = SynthetiseurMbrola.VOIX1;
-	    break;
-	case 2: 
-	    voix = SynthetiseurMbrola.VOIX2;
-	    break;
-	case 3: 
-	    voix = SynthetiseurMbrola.VOIX3;
-	    break;
-	}
+        switch (v) {
+            case 1:
+                voix = SynthetiseurMbrola.VOIX1;
+                break;
+            case 2:
+                voix = SynthetiseurMbrola.VOIX2;
+                break;
+            case 3:
+                voix = SynthetiseurMbrola.VOIX3;
+                break;
+        }
     }
-   
+
     /** Pour savoir si le texte est vide
      */
     public boolean vide() {
-	return vide;
+        return vide;
     }
 
-    /** Pour changer le texte � lire 
+    /** Pour changer le texte ï¿½ lire
      */
     public void setTexte(String s) {
-	pt = new Pretraitement(s);
-	vide = false;
+        pt = new Pretraitement(s);
+        vide = false;
     }
 
-    /** Pour recharger les fichiers de r�gles (reconstruction de l'arbre)
-     * @param pathRegle l'emplacement du repertoire contenant les r�gles
-     * @param pathPrepo l'emplacement du repertoire contenant les pr�positions (DEPRECATED)
+    /** Pour recharger les fichiers de rï¿½gles (reconstruction de l'arbre)
+     * @param pathRegle l'emplacement du repertoire contenant les rï¿½gles
+     * @param pathPrepo l'emplacement du repertoire contenant les prï¿½positions (DEPRECATED)
      */
     private void reloadArbre(String pathRegle, String pathPrepo) throws AnalyseException {
-	arbre = new Arbre(PATH_REGLE);
+        arbre = new Arbre(PATH_REGLE);
     }
 
-    /** Pour metttre � jour les r�gles de prononciation
+    /** Pour metttre ï¿½ jour les rï¿½gles de prononciation
      */
     public void reloadArbre() throws AnalyseException {
-	arbre = new Arbre(PATH_REGLE);
+        arbre = new Arbre(PATH_REGLE);
     }
 
     /** Pour lire le texte phrase par phrase avec Mbrola.
-     * @return la chaine de caract�re repr�senant la liste de phon�mes � prononcer.
+     * @return la chaine de caractï¿½re reprï¿½senant la liste de phonï¿½mes ï¿½ prononcer.
      */
     public String play() {
-	ListePhonemes l = new ListePhonemes();
-	try {
-	    Phrase p = pt.nouvellePhrase();
-	    SynthetiseurMbrola s;
-	    if (p != null) {
-		l = new ListePhonemes(arbre.trouverPhoneme(p.getPhrase()),p.getProsodie());
-		l.ecrirePhonemes(outFile + ".pho");
-		s = new SynthetiseurMbrola(voix,outFile, "", l.nombrePhonemes());
-		s.play();
+        ListePhonemes l = new ListePhonemes();
+        try {
+            Phrase p = pt.nouvellePhrase();
+            SynthetiseurMbrola s;
+            if (p != null) {
+                l = new ListePhonemes(arbre.trouverPhoneme(p.getPhrase()), p.getProsodie());
+                l.ecrirePhonemes(outFile + ".pho");
+                s = new SynthetiseurMbrola(voix, outFile, "", l.nombrePhonemes());
+                s.play();
 
-		// on laisse le temps de prononcer la phrase
-		Thread.sleep(l.dureePhonemes()+ 100);
-	    }
-	}
-	catch (PlusDePhraseException e) {
-	    vide = true;
-	}
-	catch (Exception e) {}
-	return l.toString();
+                // on laisse le temps de prononcer la phrase
+                Thread.sleep(l.dureePhonemes() + 100);
+            }
+        } catch (PlusDePhraseException e) {
+            vide = true;
+        } catch (Exception e) {
+        }
+        return l.toString();
     }
 
-    /** Pour lire la totalit� d'un texte avec Mbrola
-     * @return la chaine de caract�res repr�sentant la liste de phon�mes � prononcer.
+    /** Pour lire la totalitï¿½ d'un texte avec Mbrola
+     * @return la chaine de caractï¿½res reprï¿½sentant la liste de phonï¿½mes ï¿½ prononcer.
      */
     public String playAll() {
-	ListePhonemes l = new ListePhonemes();
-	try {
-	    Phrase p = pt.nouvellePhrase();
-	    while (true) {
-		l.ajouterPhonemes(arbre.trouverPhoneme(p.getPhrase()),p.getProsodie());
-		p = pt.nouvellePhrase();
-	    }
-	}
-	catch (PlusDePhraseException e) {
-	    vide = true;
-	    l.ecrirePhonemes(outFile + ".pho");
-	    SynthetiseurMbrola s = new SynthetiseurMbrola(voix,outFile, "", l.nombrePhonemes());
-	    s.play();
-	    try {
-		Thread.sleep(l.dureePhonemes()+ 100);
-	    } catch (Exception ee ) {};
-	}
-	catch (Exception e) {}
-	return l.toString();
+        ListePhonemes l = new ListePhonemes();
+        try {
+            Phrase p = pt.nouvellePhrase();
+            while (true) {
+                l.ajouterPhonemes(arbre.trouverPhoneme(p.getPhrase()), p.getProsodie());
+                p = pt.nouvellePhrase();
+            }
+        } catch (PlusDePhraseException e) {
+            vide = true;
+            l.ecrirePhonemes(outFile + ".pho");
+            SynthetiseurMbrola s = new SynthetiseurMbrola(voix, outFile, "", l.nombrePhonemes());
+            s.play();
+            try {
+                Thread.sleep(l.dureePhonemes() + 100);
+            } catch (Exception ee) {
+            }
+            ;
+        } catch (Exception e) {
+        }
+        return l.toString();
     }
 
     /** Pour generer un fichier sonore en mode silencieux (ne rien prononcer).
-     * <p><b>Remarque</b> : utilis� majoritairement (uniquement ?) par le serveur web de d�monstration</p>
+     * <p><b>Remarque</b> : utilisï¿½ majoritairement (uniquement ?) par le serveur web de dï¿½monstration</p>
      */
     public String muet() {
-	return this.muet(SynthetiseurMbrola.MBROLAHOME);
+        return this.muet(SynthetiseurMbrola.MBROLAHOME);
     }
-  
+
     /** MODIFIEE
-     * Pour generer un fichier de phon�me en mode silencieux (ne rien prononcer)
+     * Pour generer un fichier de phonï¿½me en mode silencieux (ne rien prononcer)
      * @param mb l'emplacement de MBROLA.
      */
     private String muet(String mb) {
-	ListePhonemes l = new ListePhonemes();
-	try {
-	    Phrase p = pt.nouvellePhrase();
-	    while (true) {
-		l.ajouterPhonemes(arbre.trouverPhoneme(p.getPhrase()),p.getProsodie());
-		p = pt.nouvellePhrase();
-		//l.ecrirePhonemes(outFile + ".pho"); // marche mal*
-	    }
-	}
-	catch (PlusDePhraseException e) {
-	    vide = true;
+        ListePhonemes l = new ListePhonemes();
+        try {
+            Phrase p = pt.nouvellePhrase();
+            while (true) {
+                l.ajouterPhonemes(arbre.trouverPhoneme(p.getPhrase()), p.getProsodie());
+                p = pt.nouvellePhrase();
+                //l.ecrirePhonemes(outFile + ".pho"); // marche mal*
+            }
+        } catch (PlusDePhraseException e) {
+            vide = true;
             l.ecrirePhonemes(outFile + ".pho"); // *correction
-	    SynthetiseurMbrola s = new SynthetiseurMbrola(mb,voix,outFile, "", l.nombrePhonemes());
-	    s.muet();
-	}
-	catch (Exception e) {}
-	return l.toString();
+            SynthetiseurMbrola s = new SynthetiseurMbrola(mb, voix, outFile, "", l.nombrePhonemes());
+            s.muet();
+        } catch (Exception e) {
+        }
+        return l.toString();
     }
 
     /** AJOUT
-     * Pour retrouver le dossier où les fichiers générés sont enregistrer
-     * @return le chemin vers les fichiers générés (sans extension)
+     * Pour retrouver le dossier oÃ¹ les fichiers gÃ©nÃ©rÃ©s sont enregistrer
+     * @return le chemin vers les fichiers gÃ©nÃ©rÃ©s (sans extension)
      */
-    public static String getPathWav() {
-        return PHO_WAV;
+    public String getPath() {
+        return outFile;
     }
 
+    /** AJOUT
+     * Pour supprimer un fichier en ayant son chemin
+     * @param pathName le chemin relatif Ã  la racine du projet
+     */
+    public void delete(String pathName) {
+        File aSuppr = new File(pathName);
+        aSuppr.delete();
+    }
 }
